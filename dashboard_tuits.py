96a13be18bea1891ca46b1c6449ce4c3dc1fa2d6
David <davidcarcon@ciencias.unam.mx>
2021-09-03 17:21:50 -0500

    Nueva gráfica pie, cambios a las network

--- dashboard_tuits.py
+++ dashboard_tuits.py
@@ -5,10 +5,21 @@ from PIL import Image
 import nltk
 import re
 import networkx as nx
+import emoji
 import matplotlib.pyplot as plt
 nltk.download('punkt')
 
-
+def quitaEmoUrlPunt(cadena):
+    new = emoji.get_emoji_regexp().sub(r'', cadena)
+    tokens = new.split()
+    palabras = []
+    for token in tokens:
+        if token.startswith('http'):
+            continue
+        else:
+            palabras.append(token)
+    salida = re.sub(r'[^\w\s]', '', " ".join(palabras))
+    return salida
 
 df_can = pd.read_csv('datos/datos_ult_dash.csv')
 df_score = pd.read_csv('datos/score_users.csv')
@@ -60,7 +71,12 @@ bar = st.beta_container()
 with bar:
     if select_event == 'Todos':
         s = df_can.PARTIDO_COALICION.value_counts().sort_values()
+        total = s.sum()
+        s_mod = s.apply(lambda x: round((x*100)/total, 2))
         y = df_can.groupby('PARTIDO_COALICION')['NOMBRE_CANDIDATO'].nunique().sort_values()
+        st.header('Porcentaje de participación por partido')
+        fig = px.pie(s_mod, values=s_mod.values, names=s_mod.index)
+        st.write(fig)
         st.header('Tuits hechos por cada Partido o Coalición')
         fig = px.bar(x=s.values, y=s.index, orientation='h',
                      labels={
@@ -79,7 +95,8 @@ with bar:
         st.write(fig2)
 
         st.header('Score generado por el número de tuits de cada candidato')
-        st.table(df_score_v.head(15).assign(hack='').set_index('hack'))
+        #st.table(df_score_v.assign(hack='').set_index('hack'))
+        st.dataframe(df_score_v.assign(hack='').set_index('hack'))
     else:
         st.header('Muestra de Tuits hechos por candidatos del partido')
         df_partido = df_can[df_can['PARTIDO_COALICION'] == select_event]
@@ -94,42 +111,51 @@ with bar:
         max_follow = max_follow.head(5).assign(hack='').set_index('hack')
         st.table(max_follow)
 
+        palabras = ['sustentable', 'desarrollo', 'ambiental', 'planeta', 'cambio',
+                    'climático', 'contaminación', 'biodiversidad', 'iniciativa', 'conciencia']
+        #### CAMBIOS
         st.header('Palabras al rededor')
-        cuerpo = " ".join(df_usuarios.Texto_tuit.to_list())
-        cuerpo = cuerpo.lower()
-        cuerpo = cuerpo.split()
+        options = st.multiselect('Escojer palabras', palabras, ['cambio'])
+        cuerpo = " ".join(df_usuarios.Texto_tuit.to_list()).lower()
+        new = quitaEmoUrlPunt(cuerpo)
+        new = new.split()
         new_cuerpo = []
-        for palabra in cuerpo:
+        for palabra in new:
             if not palabra in stopwords:
                 new_cuerpo.append(palabra)
         new_cuerpo = " ".join(new_cuerpo)
-        new_cuerpo = re.sub(r'[^\w\s]', '', new_cuerpo)
         tokens = nltk.word_tokenize(new_cuerpo)
         bgs = nltk.bigrams(tokens)
         fdist = nltk.FreqDist(bgs)
         buscar = []
-        for k,v in fdist.items():
-            if (k[0] == 'climático') or (k[1] == 'climático'):
-                buscar.append(k[0])
-                buscar.append(k[1])
-        d = set(buscar)
-        para_grafo = []
-        for k,v in fdist.items():
-            if (k[0] in d) or (k[1] in d):
-                para_grafo.append(k)
+        if len(options) > 2:
+            st.text('Lo máximo que pudes visualizar son dos')
+        else:
+            for option in options:
+                for k,v in fdist.items():
+                    if (k[0].startswith(option[:5])) or (k[1].startswith(option[:5])):
+                        buscar.append(k)
         G = nx.Graph()
-        G.add_edges_from(para_grafo[:10])
-        options = {
-            'font_size': '5',
-            'node_color': 'white',
-            'node_size': 2000,
-            'width': 1
-        }
-        #dot = nx.nx_pydot.to_pydot(G)
-        fig, ax = plt.subplots()
-        ax = nx.draw(G, with_labels=True, font_weight='bold', **options)
-        st.pyplot(fig)
+        G.add_edges_from(buscar)
 
+        fig, ax = plt.subplots(figsize=(15, 8))
+        pos = nx.spring_layout(G, k=0.4)
+
+        # Plot networks
+        nx.draw_networkx(G, pos,
+                         font_size=10,
+                         width=3,
+                         edge_color='grey',
+                         node_color='purple',
+                         with_labels = False,
+                         ax=ax)
+        for key, value in pos.items():
+            x, y = value[0]+.1, value[1]+.035
+            ax.text(x, y,
+                    s=key,
+                    bbox=dict(facecolor='white', alpha=0.2),
+                    horizontalalignment='center', fontsize=11)
+        st.pyplot(fig)
 
 st.markdown("""<style>
 .main {
